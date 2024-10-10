357e300a3b424c2c66452d30d563d37640a67a9d
Jalindar Varpe <126875635+jalindarvarpe2015@users.noreply.github.com>
2024-10-10 13:56:36 +0530

    Update MongoDB.sql
    Adding mongoDB Basics

--- MongoDB.sql
+++ MongoDB.sql
@@ -10,6 +10,9 @@ JSON is "self-describing" and easy to understand
 * The JSON syntax is derived from JavaScript object notation syntax, but the JSON format is text only. Code for reading and generating JSON data can be written in any programming language.
 
 JSON Example
+This JSON syntax defines an employees object: an array of 3 employee records (objects):
+
+JSON Example
 {
 "employees":[
   {"firstName":"John", "lastName":"Doe"},
