0bae48febd6c0f6e11b0399de42ae70c372b0526
Jalindar Varpe <126875635+jalindarvarpe2015@users.noreply.github.com>
2024-10-10 15:34:40 +0530

    Update SQL VS NoSQL.sql
    Adding basic difference between SQL nd NoSQL Databases

--- SQL VS NoSQL.sql
+++ SQL VS NoSQL.sql
@@ -1,8 +1,14 @@
-SQL vs Document Databases
-SQL databases are considered relational databases. They store related data in separate tables. When data is needed, it is queried from multiple tables to join the data back together.
+# SQL VS No-SQL
 
-MongoDB is a document database which is often referred to as a non-relational database. This does not mean that relational data cannot be stored in document databases. It means that relational data is stored differently. A better way to refer to it is as a non-tabular database.
+SQL vs Document Databases : 
+SQL databases are considered relational databases. They store related data in separate tables.
+When data is needed, it is queried from multiple tables to join the data back together.
 
-MongoDB stores data in flexible documents. Instead of having multiple tables you can simply keep all of your related data together. This makes reading your data very fast.
+MongoDB is a document database which is often referred to as a non-relational database.
+This does not mean that relational data cannot be stored in document databases. It means that relational data is stored differently.
+A better way to refer to it is as a non-tabular database.
+
+MongoDB stores data in flexible documents. Instead of having multiple tables you can simply keep all of your related data together. 
+This makes reading your data very fast.
 
 You can still have multiple groups of data too. In MongoDB, instead of tables these are called collections.
