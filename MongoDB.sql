79d03d3c8fdbc161e9caff8a2e235009e3ac1acb
Jalindar Varpe <126875635+jalindarvarpe2015@users.noreply.github.com>
2024-10-10 12:19:39 +0530

    Update MongoDB.sql
    Adding basic of MongoDB

--- MongoDB.sql
+++ MongoDB.sql
@@ -1,1 +1,33 @@
+# MongoDb 
+
+MongoDB is a document database. It stores data in a type of JSON format called BSON.
+
+What is JSON?
+JSON stands for JavaScript Object Notation
+JSON is a lightweight data interchange format
+JSON is language independent *
+JSON is "self-describing" and easy to understand
+* The JSON syntax is derived from JavaScript object notation syntax, but the JSON format is text only. Code for reading and generating JSON data can be written in any programming language.
+
+JSON Example
+{
+"employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"Anna", "lastName":"Smith"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+
+JSON Syntax Rules
+Data is in name/value pairs
+Data is separated by commas
+Curly braces hold objects
+Square brackets hold arrays
+
+
+
+
+
+
+
 
